95ac63c51ee087fcbab23239e13a5fc5bbab1077
Andistorm <andreas.heinrich@rwth-aachen.de>
2021-12-22 11:34:34 +0100

    Fix ModbusMeter

--- modules/ModbusMeter/main/powermeterImpl.cpp
+++ modules/ModbusMeter/main/powermeterImpl.cpp
@@ -17,38 +17,45 @@ void powermeterImpl::ready() {
 
 void powermeterImpl::run_meter_loop() {
     EVLOG(debug) << "Starting ModbusMeter loop";
-    int32_t power_in, power_out, energy_in, energy_out, power_surplus, energy_surplus;
+    int32_t power_in, power_out, energy_in, energy_out;
+    uint64_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     while (true) {
+        try {
+            this->tcp_conn->make_connection();
+            // Reading meter values from MODBUS device
+            power_in = (uint32_t) this->read_power_in();
+            power_out = (uint32_t) this->read_power_out();
+            energy_in = (uint32_t) this->read_energy_in();
+            energy_out = (uint32_t) this->read_energy_out();
+            this->tcp_conn->close_connection();
 
-        // Reading meter values from MODBUS device
-        power_in = (uint32_t) this->read_power_in();
-        power_out = (uint32_t) this->read_power_out();
-        energy_in = (uint32_t) this->read_energy_in();
-        energy_out = (uint32_t) this->read_energy_out();
-
-        uint64_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-
+            timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        }
+        catch (const std::exception& e) {
+            EVLOG(error) << e.what();
+            EVLOG(warning) << "Couldn't get all values from the modbus device, publish old values..";
+        }
         // Publishing relevant vars
         json j;
-        j["time_stamp"] = timestamp;
+        j["timestamp"] = timestamp;
         j["meter_id"] = "MODBUS_POWERMETER";
         j["energy_Wh_import"]["total"] = energy_in;
         j["energy_Wh_export"]["total"] = energy_out;
-
         j["power_W"]["total"] = power_in-power_out;
+        publish_powermeter(j);
+        
         std::this_thread::sleep_for(std::chrono::milliseconds(config.update_interval));
-
     }
 }
 
 void powermeterImpl::init_modbus_client() {
     this->tcp_conn = std::make_unique<everest::connection::TCPConnection>(config.modbus_ip_address, config.modbus_port);
     this->modbus_client = std::make_unique<everest::modbus::ModbusTCPClient>(*this->tcp_conn);
+    this->tcp_conn->close_connection();
 }
 
 uint32_t powermeterImpl::read_power_in() {
     std::vector<uint8_t> bytevector = this->modbus_client->read_holding_register(config.power_unit_id, config.power_in_register, config.power_in_length);
-    everest::modbus::utils::print_message_hex(bytevector);
     return everest::sunspec::conversion::bytevector_to_uint32(bytevector);
 }
 
