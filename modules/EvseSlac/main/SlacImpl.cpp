c9b93b531d1bfdb302602b0a3ef40aafcc9921eb
aw <aw@pionix.de>
2022-02-15 12:19:24 +0000

    Initial draft
    Signed-off-by: aw <aw@pionix.de>

---
+++ modules/EvseSlac/main/SlacImpl.cpp
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 - 2022 Pionix GmbH and Contributors to EVerest
+
+#include "SlacImpl.hpp"
+
+#include <future>
+
+#include <fmt/core.h>
+#include <fsm/specialization/sync/posix.hpp>
+#include <slac/channel.hpp>
+
+#include "evse_fsm.hpp"
+#include "slac_io.hpp"
+
+static SlacIO slac_io;
+static std::mutex comm_mtx;
+static std::promise<void> module_ready;
+static fsm::sync::PosixController<EvseFSM::StateHandleType> fsm_ctrl;
+static std::condition_variable new_event_cv;
+static bool new_event;
+
+// void notify() {
+//     new_event = true;
+//     new_event_cv.notify_one();
+// }
+
+bool send_event(const EvseFSM::EventInfoType::BaseType& event) {
+    std::lock_guard<std::mutex> lck(comm_mtx);
+    new_event = true;
+
+    auto ret = fsm_ctrl.submit_event(event);
+
+    new_event_cv.notify_one();
+    return ret;
+}
+
+namespace module {
+namespace main {
+
+void SlacImpl::init() {
+    // initialize static variables
+    new_event = false;
+
+    // validate config settings
+    if (config.evse_id.length() != slac::defs::STATION_ID_LEN) {
+        EVLOG_AND_THROW(
+            Everest::EverestConfigError(fmt::format("The EVSE id config needs to be exactly {} octets (got {}).",
+                                                    slac::defs::STATION_ID_LEN, config.evse_id.length())));
+    }
+
+    if (config.nid.length() != slac::defs::NID_LEN) {
+        EVLOG_AND_THROW(Everest::EverestConfigError(fmt::format(
+            "The NID config needs to be exactly {} octets (got {}).", slac::defs::NID_LEN, config.nid.length())));
+    }
+
+    // init slac socket
+    try {
+        slac_io.init(config.device);
+    } catch (const std::exception& e) {
+        Everest::EverestBaseRuntimeError(
+            fmt::format("Couldn't open device {} for SLAC communication. Reason: {}", config.device, e.what()));
+    }
+
+    // setup evse fsm thread
+    std::thread(&SlacImpl::run, this);
+}
+
+void SlacImpl::ready() {
+    // let the waiting run thread go
+    module_ready.set_value();
+}
+
+void SlacImpl::run() {
+    // initialize everything and wait for ready
+    EvseFSM fsm(slac_io);
+
+    // wait until ready
+    module_ready.get_future().get();
+
+    fsm_ctrl.reset(fsm.get_initial_state());
+
+    std::unique_lock<std::mutex> feed_lck(comm_mtx);
+    int next_timeout_in_ms = 0;
+
+    while (true) {
+        do {
+            next_timeout_in_ms = fsm_ctrl.feed();
+        } while (next_timeout_in_ms == 0);
+
+        if (next_timeout_in_ms < 0) {
+            new_event_cv.wait(feed_lck, [] { return new_event; });
+        } else {
+            new_event_cv.wait_for(feed_lck, std::chrono::milliseconds(next_timeout_in_ms), [] { return new_event; });
+        }
+
+        if (new_event) { // the if might be not necessary
+            new_event = false;
+        }
+    }
+}
+
+void SlacImpl::handle_reset() {
+    EVLOG_AND_THROW(Everest::EverestInternalError("reset() not implemented yet"));
+};
+
+bool SlacImpl::handle_enter_bcd() {
+    return send_event(EventEnterBCD());
+};
+
+bool SlacImpl::handle_leave_bcd() {
+    return send_event(EventLeaveBCD());
+};
+
+bool SlacImpl::handle_error_sequence_reset(bool& reset) {
+    if (reset) {
+        return send_event(EventErrorSequenceDone());
+    }
+
+    // otherwise, what should we do?
+    EVLOG_AND_THROW(Everest::EverestInternalError("error_sequence_reset(false) not implemented yet"));
+    return false;
+};
+
+bool SlacImpl::handle_dlink_terminate() {
+    EVLOG_AND_THROW(Everest::EverestInternalError("dlink_terminate() not implemented yet"));
+    return false;
+};
+
+bool SlacImpl::handle_dlink_error() {
+    EVLOG_AND_THROW(Everest::EverestInternalError("dlink_error() not implemented yet"));
+    return false;
+};
+
+bool SlacImpl::handle_dlink_pause() {
+    EVLOG_AND_THROW(Everest::EverestInternalError("dlink_pause() not implemented yet"));
+    return false;
+};
+
+} // namespace main
+} // namespace module
